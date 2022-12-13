32b2e5ac73afb233648274dbc50ac28260c327c9
Phan Hoai Thu <phanhoaithuabc@gmail.com>
2022-12-13 23:21:55 +0700

    phanhoaithu modify view of blog

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+posts = [
+    {
+        'author': 'CoreyMS',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'August 28, 2018'
+    }
+]
+
 def home(requests):
-    return render(requests, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(requests, 'blog/home.html', context)
 
 def about(requests):
-    return HttpResponse("<h1>about</h1>")
+    return render(requests, 'blog/about.html')
