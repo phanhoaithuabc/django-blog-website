61ed7ca9f96fed496dc6a0b01b6f9049c19e0982
Phan Hoai Thu <phanhoaithuabc@gmail.com>
2022-12-16 11:00:08 +0700

    phanhoaithu add user app into blog

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 posts = [
@@ -19,7 +20,7 @@ posts = [
 
 def home(requests):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(requests, 'blog/home.html', context)
 
